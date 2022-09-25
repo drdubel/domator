672cb5bc021cce10a619d79ee564e02cfa099acc
DrDubel <antek.suszko@gmail.com>
2022-09-25 19:45:31 +0200

    Added google authorization.

--- turbacz/turbacz/main.py
+++ turbacz/turbacz/main.py
@@ -1,28 +1,97 @@
 import logging
 import os
+import pickle
+from secrets import token_urlsafe
+from typing import Optional
 
-from fastapi import FastAPI, Response, WebSocket
+from authlib.integrations.starlette_client import OAuth, OAuthError
+from fastapi import Cookie, FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, ValidationError
+from starlette.config import Config
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.requests import Request
+from starlette.responses import HTMLResponse, RedirectResponse
 
+from .autoryzowane import authorized
 from .broker import mqtt
 from .websocket import ws_manager
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="!secret")
 mqtt.init_app(app)
+
+config = Config("turbacz/.env")
+oauth = OAuth(config)
 
 background_task_started = False
 
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
+CONF_URL = "https://accounts.google.com/.well-known/openid-configuration"
+oauth.register(
+    name="google",
+    server_metadata_url=CONF_URL,
+    client_kwargs={"scope": "openid email profile"},
+)
+
+with open('turbacz/cookies.pickle', 'rb') as cookies:
+    access_cookies = pickle.load(cookies)
+
 
 @app.get("/")
-async def main():
-    with open(os.path.join("static", "index.html")) as fh:
-        data = fh.read()
-    return Response(content=data, media_type="text/html")
+async def homepage(request: Request, access_token: Optional[str] = Cookie(None)):
+    user = request.session.get("user")
+    if access_token and access_token in access_cookies:
+        return RedirectResponse(url="/auto")
+    if user:
+        return HTMLResponse('<p>invalid email!</p><a href="/login">login</a>')
+    return HTMLResponse('<a href="/login">login</a>')
+
+
+@app.get("/auto")
+async def main(request: Request):
+    user = request.session.get("user")
+    if user:
+        with open(os.path.join("static", "index.html")) as fh:
+            data = fh.read()
+        return HTMLResponse(content=data, media_type="text/html")
+    return RedirectResponse(url="/")
+
+
+@app.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for("auth")
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.get("/auth")
+async def auth(request: Request):
+    try:
+        token = await oauth.google.authorize_access_token(request)
+    except OAuthError as error:
+        return HTMLResponse(f"<h1>{error.error}</h1>")
+    user = token.get("userinfo")
+    if user:
+        request.session["user"] = dict(user)
+        if user["email"] in authorized:
+            access_token = token_urlsafe()
+            access_cookies[access_token] = user["email"]
+            with open("turbacz/cookies.pickle", "wb") as cookies:
+                pickle.dump(access_cookies, cookies)
+            response = RedirectResponse(url="/auto")
+            response.set_cookie("access_token", access_token, max_age=3600*12)
+            return response
+        else:
+            return RedirectResponse(url="/")
+
+
+@app.get("/logout")
+async def logout(request: Request):
+    request.session.pop("user", None)
+    return RedirectResponse(url="/")
 
 
 class BlindRequest(BaseModel):
@@ -36,10 +105,9 @@ async def set_blind(req: BlindRequest):
 
 
 @app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+async def websocket_endpoint(websocket: WebSocket, access_token=Cookie()):
     await ws_manager.connect(websocket)
     mqtt.publish("/blind/cmd", "S")
-
     async def receive_command(websocket: WebSocket):
         async for cmd in websocket.iter_json():
             try:
@@ -51,7 +119,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
             mqtt.client.publish("/blind/cmd", f"{req.blind} {req.position}")
             ws_manager.command_q.put_nowait(req)
 
-    await receive_command(websocket)
+    if access_token in access_cookies:
+        await receive_command(websocket)
 
 
 def start():

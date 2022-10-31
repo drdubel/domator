9695316e8683e2ba860eddf6d10a4982b056a7cd
drdubel <antek.suszko@gmail.com>
2022-10-31 13:11:14 +0100

    remove comments

--- turbacz/blind-mover/src/main.rs
+++ turbacz/blind-mover/src/main.rs
@@ -1,7 +1,3 @@
-//! Serial interface loopback test
-//!
-//! You have to short the TX and RX pins to make this program work
-
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
@@ -19,32 +15,14 @@ use stm32f1xx_hal::{
 
 #[entry]
 fn main() -> ! {
-    // Get access to the device specific peripherals from the peripheral access crate
     let p = pac::Peripherals::take().unwrap();
-
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
     let mut flash = p.FLASH.constrain();
     let mut rcc = p.RCC.constrain();
-
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    // Prepare the alternate function I/O registers
     let mut afio = p.AFIO.constrain(&mut rcc.apb2);
-
-    // Prepare the GPIOB peripheral
     let mut gpiob = p.GPIOB.split(&mut rcc.apb2);
-
-    // USART3
-    // Configure pb10 as a push_pull output, this will be the tx pin
     let tx = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
-    // Take ownership over pb11
     let rx = gpiob.pb11;
-
-    // Set up the usart device. Taks ownership over the USART register and tx/rx pins. The rest of
-    // the registers are used to enable and configure the device.
     let serial = Serial::usart3(
         p.USART3,
         (tx, rx),
@@ -53,13 +31,10 @@ fn main() -> ! {
         clocks,
         &mut rcc.apb1,
     );
-
-
-    // You can also split the serial struct into a receiving and a transmitting part
     let (mut tx, mut rx) = serial.split();
- 
+
     loop {
         let received = block!(rx.read()).unwrap();
-        block!(tx.write(received+1)).ok();
+        block!(tx.write(received + 1)).ok();
     }
 }

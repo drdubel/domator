349b93b9d5d5e201f5e74275d10c0be806e7bd6e
drdubel <antek.suszko@gmail.com>
2024-01-20 14:44:00 +0100

    first step into google authentication

--- turbacz_app/lib/main.dart
+++ turbacz_app/lib/main.dart
@@ -1,18 +1,53 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'homepage.dart';
 
-void main() {
+late UserCredential userCredentials;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  userCredentials = await signInWithGoogle();
   runApp(const Turbacz());
 }
 
 class Turbacz extends StatefulWidget {
-  const Turbacz({Key? key}) : super(key: key);
+  const Turbacz({super.key});
 
   @override
   State<Turbacz> createState() => _Turbacz();
 }
 
+Future<UserCredential> signInWithGoogle() async {
+  final GoogleSignInAccount? googleUser =
+      await GoogleSignIn(scopes: ["email"]).signIn();
+
+  final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  print(credential);
+  print(googleAuth);
+
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
 class _Turbacz extends State<Turbacz> {
+  @override
+  void initState() {
+    super.initState();
+    print(userCredentials.user?.uid);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

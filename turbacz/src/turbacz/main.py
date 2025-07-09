f2fdc2a86e57c89359423048403620e5d850fd80
Michał Suszko <michal.suszko@dry.pl>
2025-07-09 12:37:06 +0200

    Allow remote nginx access

--- turbacz/src/turbacz/main.py
+++ turbacz/src/turbacz/main.py
@@ -160,7 +160,7 @@ def start():
     import uvicorn
 
     logging.basicConfig(level=logging.DEBUG)
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, forwarded_allow_ips="192.168.3.10")
 
 
 if __name__ == "__main__":

abd9d93e3bc564de68c2c21de1a79381dd0e05d5
drdubel <antek.suszko@gmail.com>
2025-07-09 10:33:50 +0200

    kod do przycisków

--- turbacz/wifiLightButton/src/main.cpp
+++ turbacz/wifiLightButton/src/main.cpp
@@ -3,24 +3,23 @@
 #include <WiFi.h>
 #include <credentials.h>
 
-#define NLIGHTS 3
+#define NLIGHTS 7
 
 using namespace std;
 
-char msg[2];
-const char *mqtt_broker = "192.168.3.244";
+char msg;
+const char *mqtt_broker = "10.42.0.1";
 const int mqtt_port = 1883;
-const char *mqttUser = "turbacz";
+const char *mqttUser = "switch" DEVICE_ID "-wifi";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-const int buttonPins[NLIGHTS] = {A2, A3, A4};
-int lights[NLIGHTS] = {0, 0, 0};
+const int buttonPins[NLIGHTS] = {A0, A1, A3, A4, A5, 6, 7};
+int lastTimeClick[NLIGHTS];
+int debounceDelay = 250;
 char whichLight;
-int lastButtonState = HIGH;
-
-void callback(char *topic, uint8_t *payload, int length);
+int lastButtonState[NLIGHTS] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
 
 void wifiConnect() {
     Serial.print("Connecting to ");
@@ -32,40 +31,34 @@ void wifiConnect() {
         delay(500);
     }
 
+    Serial.println(WiFi.localIP());
     Serial.println();
 }
 
 void mqttConnect() {
+    Serial.print("Connecting to MQTT broker at ");
+    Serial.print(mqtt_broker);
+    Serial.print(" with user ");
+    Serial.print(mqttUser);
+
     client.setServer(mqtt_broker, mqtt_port);
-    client.setCallback(callback);
     while (!client.connected()) {
-        if (client.connect("lights-wifi")) {
+        if (client.connect(mqttUser, mqttUser, mqttPassword)) {
+            Serial.println("Connected to MQTT broker");
         } else {
             delay(2000);
         }
-    }
-    client.subscribe("/switch/1/cmd");
-    client.publish("/switch/1/state", "jest");
-}
-
-void callback(char *topic, uint8_t *payload, int length) {
-    if ((length == 1) && ((char)payload[0] == 'S')) {
-        for (int i = 0; i < NLIGHTS; ++i) {
-            Serial.write('A' + i);
-        }
-    } else {
-        whichLight = (char)payload[0] - 'A';
-        lights[whichLight] = (char)payload[1] - '0';
     }
 }
 
 void setup() {
     Serial.begin(115200);
+
     wifiConnect();
     mqttConnect();
 
     for (int i = 0; i < NLIGHTS; i++) {
-        pinMode(buttonPins[i], INPUT_PULLUP);
+        pinMode(buttonPins[i], INPUT_PULLDOWN);
     }
 }
 
@@ -73,21 +66,24 @@ void loop() {
     for (int i = 0; i < NLIGHTS; i++) {
         int currentState = digitalRead(buttonPins[i]);
 
-        if (currentState == LOW && lastButtonState == HIGH) {
-            lights[i] = !lights[i];
-
-            msg[0] = 'a' + i;
-            msg[1] = lights[i] ? '1' : '0';
-            client.publish("/switch/1/state", msg);
-
-            delay(250);
+        if (millis() - lastTimeClick[i] < debounceDelay) {
+            continue;
         }
 
-        lastButtonState = currentState;
+        if (currentState == HIGH && lastButtonState[i] == LOW) {
+            lastTimeClick[i] = millis();
+
+            msg = 'a' + i;
+            Serial.print("Publishing message: ");
+            Serial.println(msg);
+            client.publish(("/switch/" + String(DEVICE_ID)).c_str(),
+                           String(msg).c_str());
+        }
+
+        lastButtonState[i] = currentState;
     }
 
     if (client.connected()) {
-        Serial.println("Client connected");
         client.loop();
     } else {
         mqttConnect();

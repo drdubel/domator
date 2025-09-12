6eec2f710c1b382ee31c4be4642fb824871687cd
drdubel <antek.suszko@gmail.com>
2025-09-12 22:24:44 +0200

    mesh and OTA

--- turbacz/relay8wifi/src/main.cpp
+++ turbacz/relay8wifi/src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
-#include <PubSubClient.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <HTTPClient.h>
+#include <Update.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
 #include <credentials.h>
+#include <painlessMesh.h>
+#include <webpage.h>
 
 #define USE_BOARD 75
 #define NO_PWMPIN
@@ -9,93 +15,126 @@ const byte wifiActivityPin = 255;
 
 #define NLIGHTS 8
 
-char msg[2];
-const char *mqtt_broker = "192.168.42.2";
-const int mqtt_port = 1883;
-const char *mqttUser = "relay" DEVICE_ID "-wifi";
+void receivedCallback(const uint32_t& from, const String& msg);
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+IPAddress myIP(0, 0, 0, 0);
+AsyncWebServer server(80);
+painlessMesh mesh;
+
+uint32_t rootId;
 
 int relays[NLIGHTS] = {32, 33, 25, 26, 27, 14, 12, 13};
 int lights[NLIGHTS] = {0, 0, 0, 0, 0, 0, 0, 0};
 char whichLight;
 
-void callback(char *topic, uint8_t *payload, int length);
+const char* firmware_url = "https://czupel.dry.pl/static/data/relay8wifi.bin";
 
-void wifiConnect() {
-    Serial.print("Connecting to ");
-    Serial.print(ssid);
+char message[2];
 
-    WiFi.begin(ssid, password);
+void performFirmwareUpdate() {
+    Serial.println("[OTA] Stopping mesh...");
+    mesh.stop();
+    Serial.println("[OTA] Switching to STA mode...");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(STATION_SSID, STATION_PASSWORD);
+
+    Serial.print("[OTA] Connecting to WiFi");
     while (WiFi.status() != WL_CONNECTED) {
+        delay(300);
         Serial.print(".");
-        delay(500);
     }
+    Serial.println(" connected!");
 
-    Serial.println(WiFi.localIP());
-    Serial.println();
-}
+    WiFiClientSecure client;
+    client.setInsecure();
+    HTTPClient http;
 
-void mqttConnect() {
-    Serial.print("Connecting to MQTT broker at ");
-    Serial.print(mqtt_broker);
-    Serial.print(" with user ");
-    Serial.println(mqttUser);
+    Serial.println("[OTA] Connecting to update server...");
+    if (http.begin(client, firmware_url)) {
+        int httpCode = http.GET();
+        if (httpCode == HTTP_CODE_OK) {
+            int contentLength = http.getSize();
+            Serial.printf("[OTA] Firmware size: %d bytes\n", contentLength);
 
-    client.setCallback(callback);
-    client.setServer(mqtt_broker, mqtt_port);
+            if (Update.begin(contentLength)) {
+                Serial.println("[OTA] Writing firmware...");
+                size_t written = Update.writeStream(*http.getStreamPtr());
 
-    while (!client.connected()) {
-        if (client.connect(mqttUser, mqttUser, mqttPassword)) {
-            Serial.println("Connected to MQTT broker");
+                Serial.printf("[OTA] Written %d/%d bytes\n", (int)written,
+                              contentLength);
+
+                if (Update.end()) {
+                    if (Update.isFinished()) {
+                        Serial.println("[OTA] Update finished, restarting...");
+                        ESP.restart();
+                    } else {
+                        Serial.println(
+                            "[OTA] Update not finished, something went wrong.");
+                    }
+                } else {
+                    Serial.printf("[OTA] Update error: %d\n",
+                                  Update.getError());
+                }
+            } else {
+                Serial.println("[OTA] Not enough space for OTA.");
+            }
         } else {
-            delay(2000);
+            Serial.printf("[OTA] HTTP GET failed, code: %d\n", httpCode);
         }
+        http.end();
+    } else {
+        Serial.println("[OTA] Unable to connect to update server!");
     }
-
-    client.subscribe("/relay/" DEVICE_ID "/cmd");
 }
 
-void callback(char *topic, uint8_t *payload, int length) {
-    if ((length == 1) && ((char)payload[0] == 'S')) {
+void meshInit() {
+    mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
+
+    mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6);
+    mesh.onReceive(&receivedCallback);
+
+    Serial.printf("Mesh started with ID %u\n", mesh.getNodeId());
+}
+
+void receivedCallback(const uint32_t& from, const String& msg) {
+    Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
+
+    if (msg == "S") {
         for (int i = 0; i < NLIGHTS; ++i) {
-            msg[0] = 'A' + i;
-            msg[1] = lights[i] ? '1' : '0';
+            message[0] = 'A' + i;
+            message[1] = lights[i] ? '1' : '0';
 
-            client.publish("/relay/" DEVICE_ID "/state", msg);
+            mesh.sendSingle(from, String(message, 2));
         }
-    } else if (payload[0] >= 'a' && payload[0] < 'a' + NLIGHTS && length == 2) {
-        Serial.print("Received message: ");
-        Serial.print(topic);
-        Serial.print(" with payload: ");
-
-        for (int i = 0; i < length; ++i) {
-            Serial.print((char)payload[i]);
-        }
-        Serial.println();
-
-        whichLight = (char)payload[0] - 'a';
-        lights[whichLight] = (char)payload[1] - '0';
+    } else if (msg[0] >= 'a' && msg[0] < 'a' + NLIGHTS && msg.length() == 2) {
+        whichLight = (char)msg[0] - 'a';
+        lights[whichLight] = (char)msg[1] - '0';
 
         digitalWrite(relays[whichLight], lights[whichLight] ? HIGH : LOW);
 
-        msg[0] = 'A' + whichLight;
-        msg[1] = lights[whichLight] ? '1' : '0';
-        client.publish("/relay/" DEVICE_ID "/state", msg);
+        message[0] = 'A' + whichLight;
+        message[1] = lights[whichLight] ? '1' : '0';
+        mesh.sendSingle(from, String(message, 2));
 
         Serial.print("Light ");
         Serial.print('a' + whichLight);
         Serial.print(" set to ");
         Serial.println(lights[whichLight] ? "ON" : "OFF");
+    } else if (msg == "U") {
+        performFirmwareUpdate();
+    } else {
+        rootId = msg.toInt();
+        Serial.print("New root ID: ");
+        Serial.println(rootId);
+
+        mesh.sendSingle(from, "R");
     }
 }
 
 void setup() {
     Serial.begin(115200);
 
-    wifiConnect();
-    mqttConnect();
+    meshInit();
 
     for (int i = 0; i < NLIGHTS; i++) {
         pinMode(relays[i], OUTPUT);
@@ -106,12 +145,4 @@ void setup() {
     digitalWrite(23, LOW);
 }
 
-void loop() {
-    if (client.connected()) {
-        digitalWrite(23, HIGH);
-        client.loop();
-    } else {
-        digitalWrite(23, LOW);
-        mqttConnect();
-    }
-}
+void loop() { mesh.update(); }

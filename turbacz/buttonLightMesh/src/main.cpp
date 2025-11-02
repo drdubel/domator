c27247efde32011e7853db84609f5d43d9c70d2e
drdubel <antek.suszko@gmail.com>
2025-11-02 15:19:44 +0100

    wifi version fallback

--- turbacz/buttonLightMesh/src/main.cpp
+++ turbacz/buttonLightMesh/src/main.cpp
@@ -3,6 +3,7 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <HTTPClient.h>
+#include <PubSubClient.h>
 #include <Update.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -13,15 +14,20 @@
 #define LED_PIN 8
 #define NUM_LEDS 1
 
-void receivedCallback(const uint32_t& from, const String& msg);
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+
+IPAddress mqtt_broker(192, 168, 3, 10);
+const int mqtt_port = 1883;
 
 Adafruit_NeoPixel pixels(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 IPAddress myIP(0, 0, 0, 0);
 AsyncWebServer server(80);
-painlessMesh mesh;
 
-uint32_t rootId;
+const char* DEVICE_ID = "1234";
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(mqtt_broker, mqtt_port, wifiClient);
 
 const int buttonPins[NLIGHTS] = {A0, A1, A3, A4, A5, 6, 7};
 int lastTimeClick[NLIGHTS];
@@ -51,10 +57,9 @@ void updateLedStatus() {
 
 void performFirmwareUpdate() {
     Serial.println("[OTA] Stopping mesh...");
-    mesh.stop();
     Serial.println("[OTA] Switching to STA mode...");
     WiFi.mode(WIFI_STA);
-    WiFi.begin(STATION_SSID, STATION_PASSWORD);
+    WiFi.begin(SSID, PASSWORD);
 
     Serial.print("[OTA] Connecting to WiFi");
     while (WiFi.status() != WL_CONNECTED) {
@@ -105,27 +110,60 @@ void performFirmwareUpdate() {
     }
 }
 
-void meshInit() {
-    mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
+void setupWiFi() {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASSWORD);
 
-    mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6);
-    mesh.onReceive(&receivedCallback);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println(" connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
-void receivedCallback(const uint32_t& from, const String& msg) {
-    Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
+void mqttConnect() {
+    Serial.print("Connecting to MQTT broker at ");
+    Serial.print(mqtt_broker);
+    Serial.print(" with user ");
+    Serial.println(MQTT_USER);
+    mqttClient.setCallback(mqttCallback);
+
+    while (!mqttClient.connected()) {
+        if (mqttClient.connect(DEVICE_ID, MQTT_USER, MQTT_PASSWORD)) {
+            Serial.println("Connected to MQTT broker");
+            digitalWrite(LED_BUILTIN, HIGH);
+        } else {
+            static unsigned long lastToggle = 0;
+            if (millis() - lastToggle > 500) {
+                digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+                lastToggle = millis();
+            }
+        }
+    }
+
+    String topic = "/switch/" + String(DEVICE_ID);
+    mqttClient.publish(topic.c_str(), "connected");
+    mqttClient.subscribe("/switch/cmd");
+
+    setLedColor(0, 255, 0);
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+    String msg;
+    for (unsigned int i = 0; i < length; i++) {
+        msg += (char)payload[i];
+    }
+
+    Serial.printf("MQTT: Received message on %s: %s\n", topic, msg.c_str());
 
     if (msg == "U") {
         setLedColor(0, 0, 255);
         performFirmwareUpdate();
         return;
     }
-
-    rootId = msg.toInt();
-    Serial.print("New root ID: ");
-    Serial.println(rootId);
-
-    mesh.sendSingle(from, "S");
 }
 
 void setup() {
@@ -134,7 +172,10 @@ void setup() {
     pixels.begin();
     pixels.setBrightness(5);
 
-    meshInit();
+    Serial.println(DEVICE_ID);
+
+    setupWiFi();
+    mqttConnect();
 
     for (int i = 0; i < NLIGHTS; i++) {
         pinMode(buttonPins[i], INPUT_PULLDOWN);
@@ -142,8 +183,6 @@ void setup() {
 }
 
 void loop() {
-    mesh.update();
-
     for (int i = 0; i < NLIGHTS; i++) {
         int currentState = digitalRead(buttonPins[i]);
 
@@ -158,9 +197,16 @@ void loop() {
             Serial.print("Publishing message: ");
             Serial.println(msg);
 
-            mesh.sendSingle(rootId, String(msg));
+            String topic = "/switch/" + String(DEVICE_ID);
+            mqttClient.publish(topic.c_str(), String(msg).c_str());
         }
 
         lastButtonState[i] = currentState;
+    }
+
+    if (!mqttClient.connected()) {
+        mqttConnect();
+    } else {
+        mqttClient.loop();
     }
 }

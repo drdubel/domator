946a4bf31d8e44153332f5db19cf58f2d35f8d9f
drdubel <antek.suszko@gmail.com>
2025-12-27 19:16:30 +0100

    fix: Update firmware URLs to point to the new server location

--- uc/relayLightMesh/src/main.cpp
+++ uc/relayLightMesh/src/main.cpp
@@ -50,7 +50,7 @@ unsigned long lastStatusReport = 0;
 String fw_md5;  // MD5 of the firmware as flashed
 
 const char* firmware_url =
-    "https://czupel.dry.pl/static/data/relay/firmware.bin";
+    "https://turbacz.dry.pl/static/data/relay/firmware.bin";
 
 void performFirmwareUpdate() {
     vTaskDelay(1000 / portTICK_PERIOD_MS);  // Allow message to be sent

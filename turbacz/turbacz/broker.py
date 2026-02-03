13e3c754cb1c0db10034fae9a73f306415581f28
drdubel <antek.suszko@gmail.com>
2026-02-03 15:03:20 +0100

    Fix relay name extraction: update to retrieve name from tuple in handle_root_state

--- turbacz/turbacz/broker.py
+++ turbacz/turbacz/broker.py
@@ -200,7 +200,7 @@ async def handle_root_state(payload_str):
 
     elif data["type"] == "relay":
         if data["deviceId"] in relays:
-            data["name"] = relays[data["deviceId"]]
+            data["name"] = relays[data["deviceId"]][0]  # Extract name from tuple
         else:
             name = namer.generate(category="animals")
             data["name"] = name
@@ -225,7 +225,7 @@ async def handle_root_state(payload_str):
     data["name"] = data["name"].replace(" ", "\\ ")
 
     if data["parentId"] in relays:
-        parent_name = relays[data["parentId"]]
+        parent_name = relays[data["parentId"]][0]  # Extract name from tuple
 
     elif data["parentId"] in switches:
         parent_name = switches[data["parentId"]][0]
